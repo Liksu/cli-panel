a64d69407ce51b0cd0268a44ceff58b9ec4722d0
Liksu <github@Liksu.kiev.ua>
2015-10-07 01:44:30 +0300

    Add help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,13 @@
 angular.module('cli').run(function($cli) {
-	$cli.command('help', 'Display this list of available commands', function(commandObject) {
+	$cli.command('help', 'Display this list', function(commandObject) {
+		$cli.print('List of available commands:');
+		Object.keys($cli.workers.commands)
+			.sort()
+			.forEach(function(command) {
+				var descr = $cli.workers.commands[command].description;
+				$cli.print(['\t', command, descr ? '- ' + descr : ''].join(' '));
+			});
+
 		console.log('in help', commandObject);
 	});
 	//console.log( 'help', angular.mode('cli')._invokeQueue );
