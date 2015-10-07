aa3d9a22db9b7b63203d1e0c761a2c5883e6bc6e
Liksu <peb@ciklum.com>
2015-10-07 14:14:16 +0300

    Fix min

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-console.log('app!', angular.module('cli'));
 angular
 	.module('app', ['cli'])
 	.controller('main', function($scope) {
-		console.log('main run');
 	});
-console.log('app second!');
