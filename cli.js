2f5471766270c3294f3e05e0ed78c42a668ebf17
Liksu <peb@ciklum.com>
2015-10-07 18:40:58 +0300

    Add services to commands

--- cli.js
+++ cli.js
@@ -193,6 +193,17 @@ angular.module('cli').run(["$cli", function($cli) {
 }]);
 
 angular.module('cli').run(["$cli", function($cli) {
+	var services = angular.modules
+		.filter(function(module) { return module !== 'cli' })
+		.reduce(function(list, module) { return list.concat(angular.module(module)._invokeQueue) }, [])
+		.filter(function(item) { return item[1] === 'service' })
+		.map(function(item) { return item[2][0] })
+		.forEach(function(service) {
+			//$cli.command(service, )
+		})
+}]);
+
+angular.module('cli').run(["$cli", function($cli) {
 	function calc(string) {
 		return eval(string);
 	}
