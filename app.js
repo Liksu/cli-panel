d04f984eea16d9e9d3b84d6617a59fbb170f4a91
Liksu <github@Liksu.kiev.ua>
2015-11-04 03:08:58 +0200

    v0.2.0

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-angular
-	.module('app', ['cli'])
-	.controller('main', function($scope) {
+var getList = function() {
+	return new Promise(function(resolve) {
+		setTimeout(function() {
+			resolve([
+				{title: 'Red', price: 152},
+				{title: 'Blue', price: 236},
+				{title: 'Green', price: 212}
+			]);
+		}, 1000);
 	});
+};
+
+// example of self command
+cli.command('books_load', 'App command', function() {
+	return getList().then(function(list) {
+		cli.print(list.map(function(book) {return book.title + ': ' + book.price + '$'}).join(', '));
+	})
+});
+
+// example of prompt change
+var updatePrompt = function() {
+	cli.cache.prompt.innerHTML = '<span class="bracket">[</span>'
+		+ '<span class="time">'
+		+ new Date().toJSON().replace(/^.+T([\d:]+).+$/, '$1')
+		+ '</span>'
+		+ '<span class="bracket">]</span>&nbsp;'
+		+ cli.settings.prompt
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	updatePrompt();
+	setInterval(updatePrompt, 1000);
+});
